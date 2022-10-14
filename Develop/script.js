bcbb4142395f82ce0ad6e6d8943a7eef298748b6
Bailey Mejia <baileymejia28@gmail.com>
2022-10-14 15:05:18 -0700

    finally found out what a time block is and added it to the HTML now i have to find out how to make a save icon

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,8 @@
 var hourOfDay = document.querySelector('#hour-ofday');
-var hours = ['9', '10', '11', '12', '1', '2', '3', '4', '5'];
-var hoursMath = Math.floor( hours.length);
 // The current day needs to displayed above the calandar
 var momentDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a"); 
 $('#currentDay').text(momentDate);
 
 
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// Use a if statement for changing colors of the time 
-// if 9am is true make color red else if that is not true make it gray
 
