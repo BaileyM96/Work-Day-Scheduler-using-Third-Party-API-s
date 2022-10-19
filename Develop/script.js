665e108a6af1d177e0b4c93892a6d87f18cf1064
Bailey Mejia <baileymejia28@gmail.com>
2022-10-18 17:49:29 -0700

    Fixed bug with colors not displaying right, added functions to add text with localStorage

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,40 @@
-var hourOfDay = document.querySelector('#hour-ofday');
-// The current day needs to displayed above the calandar
-var momentDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a"); 
+
+var momentDate = moment().format("dddd, MMMM Do YYYY"); 
 $('#currentDay').text(momentDate);
+$('.saveBtn').on('click',function() {
+    console.log('Hello');
+    var hour = $(this).parent().attr('id');
+    var text = $(this).siblings('textarea').val();
+    localStorage.setItem(hour, text);
+});
 
 
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
+function timeBlocks() {
+    var currentHour = moment().hour(); //Moment.hour displays military time
+    $('.time-block').each(function () {
+        var timeBlock = parseInt($(this).attr('id'));  
+     console.log(timeBlock);
+
+        if (timeBlock === currentHour) {
+            $(this).addClass('present');
+
+        }
+        else if (timeBlock < currentHour) {
+            $(this).addClass('past');
+        }
+        else {
+            $(this).addClass('future');
+        }
+    })
+    
+};
+
+timeBlocks();
+
+
+$('.time-block').each(function () {
+  var key =  $(this).attr('id');
+  $(this).children('textarea').val(localStorage.getItem(key));
+})
 
